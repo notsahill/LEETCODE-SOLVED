1f4dab9930b49baacf33b8521d28b0b9b7eba1b8
notsahill <90394600+notsahill@users.noreply.github.com>
2023-05-19 10:23:20 +0530

    Time: 0 ms (100.00%) | Memory: 5.9 MB (96.36%) - LeetSync

--- the-kth-factor-of-n/the-kth-factor-of-n.cpp
+++ the-kth-factor-of-n/the-kth-factor-of-n.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int kthFactor(int n, int k) 
     {
-        // vector<vector<int>> sieve(n+1);
         for(int i=1;i<=n&&k>0;i++)
         {
             if(n%i==0) k--;
