6ff7a8f0f25d848dda326963f077f1ce1ebb594c
notsahill <90394600+notsahill@users.noreply.github.com>
2023-05-19 11:34:31 +0530

    Time: 1085 ms (14.06%) | Memory: 89.9 MB (29.52%) - LeetSync

--- k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
+++ k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     typedef pair<float,pair<int,int>> pi;
-    // #define pair<double,pair<int,int>> pdpii
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) 
     {
         priority_queue<pi,vector<pi>,greater<pi> > q;
@@ -11,19 +10,10 @@ public:
             for(int j=i+1;j<n;j++)
             {
                 float f=(arr[i]*1.0)/arr[j];
-                // cout<<f<<" "<<arr[i]<<","<<arr[j]<<endl;
                 q.push({f,{arr[i],arr[j]}});
             }
         }
-        // cout<<endl<<endl;
         vector<int> ans;
-        // while(q.size())
-        // {
-        //     int i=q.top().second.first;
-        //     int j=q.top().second.second;
-        //     cout<<q.top().first<<" "<<i<<","<<j<<endl;
-        //     q.pop();
-        // }
         while(k--)
         {
             int i=q.top().second.first;
