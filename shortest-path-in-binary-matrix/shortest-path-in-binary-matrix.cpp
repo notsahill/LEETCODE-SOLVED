6e2612dab8a668610660be2b5c2a0bc7a5b943a3
notsahill <90394600+notsahill@users.noreply.github.com>
2023-06-02 02:39:39 +0530

    Time: 79 ms (55.41%) | Memory: 20.8 MB (61.89%) - LeetSync

--- shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -3,59 +3,66 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) 
     {
         int n=grid.size();
-        queue<pair<pair<int,int>,int>> q;
+        queue<pair<int,int>> q;
         vector<vector<int>> vis(n,vector<int> (n,0));
         if(grid[0][0]==0) 
         {
-            q.push({{0,0},1});
+            q.push({0,0});
             vis[0][0]=1;
         }
+        int d=1;
         while(q.size())
         {
-            int x=q.front().first.first,y=q.front().first.second,d=q.front().second;
-            vis[x][y]=1;
-                if(x==n-1 and y==n-1) return d;
+            int sz=q.size();
+            for(int i=0;i<sz;i++)
+            {
+                int x=q.front().first,y=q.front().second;
                 q.pop();
+
+                if(x==n-1 and y==n-1) return d;
+                
                 if(x-1>=0 and grid[x-1][y]==0 and !vis[x-1][y]) 
                 {
-                    q.push({{x-1,y},d+1}); 
+                    q.push({x-1,y}); 
                     vis[x-1][y]=1;
                 }
                 if(y-1>=0 and grid[x][y-1]==0 and !vis[x][y-1]) 
                 {
-                    q.push({{x,y-1},d+1});
+                    q.push({x,y-1});
                     vis[x][y-1]=1;
                 }
                 if(x+1<n and grid[x+1][y]==0 and !vis[x+1][y]) 
                 {
-                    q.push({{x+1,y},d+1}); 
+                    q.push({x+1,y}); 
                     vis[x+1][y]=1;
                 }
                 if(y+1<n and grid[x][y+1]==0 and !vis[x][y+1]) 
                 {
-                    q.push({{x,y+1},d+1});
+                    q.push({x,y+1});
                     vis[x][y+1]=1;
                 }
                 if(x-1>=0 and y-1>=0 and grid[x-1][y-1]==0 and !vis[x-1][y-1]) 
                 {
-                    q.push({{x-1,y-1},d+1}); 
+                    q.push({x-1,y-1}); 
                     vis[x-1][y-1]=1;
                 }
                 if(x-1>=0 and y+1<n and grid[x-1][y+1]==0 and !vis[x-1][y+1]) 
                 {
-                    q.push({{x-1,y+1},d+1});
+                    q.push({x-1,y+1});
                     vis[x-1][y+1]=1;
                 }
                 if(x+1<n and y-1>=0 and grid[x+1][y-1]==0 and !vis[x+1][y-1]) 
                 {
-                    q.push({{x+1,y-1},d+1});
+                    q.push({x+1,y-1});
                     vis[x+1][y-1]=1;
                 }
                 if(x+1<n and y+1<n and grid[x+1][y+1]==0 and !vis[x+1][y+1]) 
                 {
-                    q.push({{x+1,y+1},d+1});  
+                    q.push({x+1,y+1});  
                     vis[x+1][y+1]=1;
-                }       
+                }
+            }
+            d++;
         }
         return -1;
     }
