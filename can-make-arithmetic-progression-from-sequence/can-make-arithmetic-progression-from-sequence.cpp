e4d449a479bdd8488756bad1c4d96eefbf7c79cb
notsahill <90394600+notsahill@users.noreply.github.com>
2023-06-06 18:55:23 +0530

    Time: 3 ms (85.28%) | Memory: 9.2 MB (28.46%) - LeetSync

--- can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.cpp
+++ can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.cpp
@@ -2,19 +2,11 @@ class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr) 
     {
-        set<int> s;
-        int mn=INT_MAX,mx=INT_MIN,n=arr.size();
-        for(auto a:arr) 
+        sort(arr.begin(),arr.end());
+        int d=arr[1]-arr[0];
+        for(int i=2;i<arr.size();i++)
         {
-            s.insert(a);
-            mn=min(mn,a);
-            mx=max(mx,a);
-        }
-        int d=(mx-mn)/(n-1);
-        if((mx-mn)%(n-1)!=0) return 0;
-        for(int i=0;i<=n-1;i++)
-        {
-            if(!s.count(mn+(i)*d)) return 0;
+            if(arr[i]-arr[i-1]!=d) return 0;
         }
         return 1;
     }
