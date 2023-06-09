87b1e71c86dafcbe18ff883ac4d86a976815dc76
notsahill <90394600+notsahill@users.noreply.github.com>
2023-06-10 03:09:05 +0530

    Time: 17 ms (39.78%) | Memory: 14 MB (16.80%) - LeetSync

--- group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
+++ group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
@@ -10,18 +10,17 @@ public:
             int sz=groupSizes[i];
             m[sz].push_back(i);
         }
-        cout<<m.size();
         for(auto a:m)
         {
             int sz=a.first;
             int sz1=a.second.size();
-            int k=0;
+            
             for(int i=0;i<sz1/sz;i++)
             {
                 vector<int> temp;
                 for(int j=0;j<sz;j++)
                 {
-                    temp.push_back(a.second[j]);
+                    temp.push_back(a.second[j+(sz*i)]);
                 }
                 ans.push_back(temp);
             }
