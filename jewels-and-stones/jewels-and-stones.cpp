aa03775db75666e4d651c2f5504d657b8554cd4b
notsahill <90394600+notsahill@users.noreply.github.com>
2023-06-10 19:34:54 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (36.35%) - LeetSync

--- jewels-and-stones/jewels-and-stones.cpp
+++ jewels-and-stones/jewels-and-stones.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
         int count = 0;
+        set<int> s;
         for(char ch1 : jewels){
-            for(char ch2 : stones){
-                if(ch1 == ch2)
-                    count++;
-            }
+            s.insert(ch1);
+        }
+        for(auto a:stones)
+        {
+            if(s.count(a)) count++;
         }
         return count;
     }
