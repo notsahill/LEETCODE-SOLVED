e65951f30ccca7746d41c9a93b00959a2c044090
notsahill <90394600+notsahill@users.noreply.github.com>
2023-06-13 10:05:18 +0530

    Time: 292 ms (25.48%) | Memory: 388.1 MB (5.02%) - LeetSync

--- equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
+++ equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -4,17 +4,10 @@ public:
     {
         unordered_map<int,vector<int>> r,c;
         int ans=0,n=grid.size();
-        for(int i=0;i<n;i++)
-        {
-            vector<int> row;
-            for(int j=0;j<n;j++)
-            {
-                row.push_back(grid[i][j]);
-            }
-            r[i]=row;
-        }
         for(int j=0;j<n;j++)
         {
+            vector<int> row=grid[j];
+            r[j]=row;
             vector<int> col;
             for(int i=0;i<n;i++)
             {
@@ -24,13 +17,9 @@ public:
         }
         for(auto row:r)
         {
-            int idx=row.first;
-            vector<int> rw=row.second;
             for(auto col:c)
             {
-                int idx1=col.first;
-                vector<int> cl=col.second;
-                if(rw==cl) ans++;
+                if(row.second==col.second) ans++;
             }
         }
         return ans;
