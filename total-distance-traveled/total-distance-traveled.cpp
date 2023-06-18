f6b317c161a1e9c5f0834588cf938b587b58f2c8
notsahill <90394600+notsahill@users.noreply.github.com>
2023-06-18 20:44:53 +0530

    Time: 12 ms (100.00%) | Memory: 5.9 MB (88.89%) - LeetSync

--- total-distance-traveled/total-distance-traveled.cpp
+++ total-distance-traveled/total-distance-traveled.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int distanceTraveled(int mainTank, int additionalTank) 
     {
-        if(mainTank<5) return mainTank*10;
-        if(additionalTank<1) return mainTank*10;
+        if(mainTank<5 or additionalTank<1) return mainTank*10;
         return distanceTraveled(mainTank-5+1,additionalTank-1)+50;
     }
 };
