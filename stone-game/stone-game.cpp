c0c7cf5a858543e07ea9230418af74fdee03b8e5
notsahill <90394600+notsahill@users.noreply.github.com>
2023-06-28 20:16:06 +0530

    Time: 7 ms (49.40%) | Memory: 17.9 MB (9.82%) - LeetSync

--- stone-game/stone-game.cpp
+++ stone-game/stone-game.cpp
@@ -1,9 +1,23 @@
 class Solution {
-public:
+public: 
+    int dp[501][501];
+    int helper(vector<int> piles,int l,int h)
+    {
+      int n=piles.size();
+      if(l>h) return 0;
+      if(l==n-1) return piles[l];
+      if(h==0) return piles[h];
+      if(dp[l][h]!=-1) return dp[l][h];
+      return dp[l][h]=max(piles[l]+helper(piles,l+1,h-1),piles[h]+helper(piles,l+1,h-1));
+
+    }
     bool stoneGame(vector<int>& piles) 
     {
+        memset(dp,-1,sizeof(dp));
         int n=piles.size();
-        if(n%2==0) return true;
-        return false;
+        int sum=0;
+        accumulate(piles.begin(),piles.end(),sum);
+        int q=helper(piles,0,n-1);
+        return q>sum-q;
     }
 };
