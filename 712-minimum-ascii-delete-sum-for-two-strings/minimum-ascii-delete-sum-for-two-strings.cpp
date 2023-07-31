f9f77330c78f9b0ec10084607f86cdefb5c2b66f
notsahill <90394600+notsahill@users.noreply.github.com>
2023-07-31 13:53:25 +0530

    Time: 48 ms (65.73%) | Memory: 15.2 MB (68.25%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -22,9 +22,9 @@ public:
             }
             return ans;
         }
-
-        if(dp[i][j]!=-1) return dp[i][j];
+        
         if(s1[i]==s2[j]) return helper(i+1,s1,j+1,s2,dp);
+        if(dp[i][j]!=-1) return dp[i][j];
         int x= (int)s1[i] + helper(i+1,s1,j,s2,dp);
         int y=(int)s2[j] + helper(i,s1,j+1,s2,dp);
         return dp[i][j]=min(x,y);
