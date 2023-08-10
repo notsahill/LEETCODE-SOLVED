538b360152294456dd21d24c4e784fd7d0fedda9
notsahill <90394600+notsahill@users.noreply.github.com>
2023-08-11 00:08:02 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (50.91%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    int search(vector<int>& n, int k) 
+    int pivot(vector<int> &nums)
     {
-       int l=0;
-       int h=n.size()-1;
-       while(l<=h)
-       {
-           int mid=l+(h-l)/2;
-           if(n[mid]==k) 
-           {
-               return mid;
-           }
-           if((n[mid]>=n[l]))
-           {
-               {
-                   if(k>=n[l]&&k<n[mid]) h=mid-1;
-                   else
-                   l=mid+1;
-               }
-           }
-           else
-           {
-               if(k>n[mid]&&k<=n[h]) l=mid+1;
-               else 
-               h=mid-1;
-           }
-       }
+        int l=0,h=nums.size()-1;
+        while(l<h)
+        {
+            int mid=(l+h)>>1;
+            if(nums[mid]>nums[h])
+            {
+                l=mid+1;
+            }
+            else h=mid;
+        }
+        return l;
+    }
+    int bs(vector<int> &nums,int k,int l,int h)
+    {
+        while(l<=h)
+        {
+            int mid=(l+h)>>1;
+            if(nums[mid]==k) return mid;
+            if(nums[mid]>k) h=h-1;
+            else l=l+1;
+        }
         return -1;
+    }
+    int search(vector<int>& nums, int target) {
+        int pvt= pivot(nums);
+        int x=bs(nums,target,0,pvt-1);
+        if(x!=-1) return x;
+        return bs(nums,target,pvt,nums.size()-1);
     }
 };
