16eb1ee2d304849e14b35f356db120bfad6f85eb
notsahill <90394600+notsahill@users.noreply.github.com>
2023-09-08 12:42:49 +0530

    Time: 11 ms (66.99%) | Memory: 9 MB (65.21%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int h(int i,int j, string &w1,string &w2,vector<vector<int>> &dp)
+    int h(int i,int j,string &w1,string &w2,vector<vector<int>> &dp)
     {
         if(i>=w1.size()) return w2.size()-j;
         if(j>=w2.size()) return w1.size()-i;
         if(dp[i][j]!=-1) return dp[i][j];
         if(w1[i]==w2[j]) return h(i+1,j+1,w1,w2,dp);
-        int del=h(i+1,j,w1,w2,dp)+1;
-        int ins=h(i,j+1,w1,w2,dp)+1;
-        int rep=h(i+1,j+1,w1,w2,dp)+1;
-        return dp[i][j]=min(del,min(ins,rep));
+        int ins=h(i,j+1,w1,w2,dp);
+        int del=h(i+1,j,w1,w2,dp);
+        int rep=h(i+1,j+1,w1,w2,dp);
+        return dp[i][j]=min(rep,min(ins,del))+1;
     }
-    int minDistance(string w1, string w2) {
-        vector<vector<int>> dp(w1.size()+1,vector<int> (w2.size(),-1));
-        return h(0,0,w1,w2,dp);
+    int minDistance(string word1, string word2) {
+        vector<vector<int>> dp(word1.size(),vector<int> (word2.size(),-1));
+        return h(0,0,word1,word2,dp);
     }
 };
