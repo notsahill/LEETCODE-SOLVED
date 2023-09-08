39d6e915060b19ca01e447e5aaa2aa017a5ab3c9
notsahill <90394600+notsahill@users.noreply.github.com>
2023-09-08 11:18:45 +0530

    Time: 261 ms (54.65%) | Memory: 95.8 MB (23.49%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    bool h(int idx,int curr_sum,int &total,vector<int> &nums,vector<vector<int>> &dp)
+    int h(int idx,int reqd,vector<int> &nums,vector<vector<int>> &dp)
     {
         if(idx>=nums.size()) return 0;
-        if(curr_sum==total-curr_sum) return true;
-        if(dp[idx][curr_sum]!=-1) return dp[idx][curr_sum];
-        int inc=h(idx+1,curr_sum+nums[idx],total,nums,dp);
-        int exc=h(idx+1,curr_sum,total,nums,dp);
-        return dp[idx][curr_sum]=inc||exc;
+        if(reqd==0) return 1;
+        int ans=0;
+        if(dp[idx][reqd]!=-1) return dp[idx][reqd];
+        if(reqd>=nums[idx]) ans|=h(idx+1,reqd-nums[idx],nums,dp);
+        ans|=h(idx+1,reqd,nums,dp);
+        return dp[idx][reqd]=ans;
     }
     bool canPartition(vector<int>& nums) {
-        int total=0;
-        for(auto a:nums) total+=a;
-        if(total%2!=0) return false;
-        vector<vector<int>> dp(nums.size()+1,vector<int> (20001,-1));
-        return h(0,0,total,nums,dp);    
+        int sum=0,n=nums.size();
+        for(auto a:nums) sum+=a;
+        if(sum%2!=0 or n==1) return false;
+        vector<vector<int>> dp(nums.size()+1,vector<int> (sum/2+1,-1));
+        return h(0,sum/2,nums,dp);
     }
 };
