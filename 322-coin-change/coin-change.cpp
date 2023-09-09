d499208201200f5cd3d491d17e8898f87c03b27c
notsahill <90394600+notsahill@users.noreply.github.com>
2023-09-09 10:00:46 +0530

    Time: 56 ms (88.86%) | Memory: 49.5 MB (12.35%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,24 +2,23 @@ class Solution {
 public:
     int h(int idx,int amount,vector<int> &coins,vector<vector<int>> &dp)
     {
-        if(idx<0) return 0;
+        if(idx<0) return INT_MAX-1;
         if(idx==0)
         {
             if(amount%coins[idx]==0) return amount/coins[idx];
             return INT_MAX-1;
         }
         if(dp[idx][amount]!=-1) return dp[idx][amount];
-        int a=INT_MAX-1;
-        if(coins[idx]<=amount) a=1+h(idx,amount-coins[idx],coins,dp);
-        int b=h(idx-1,amount,coins,dp);
-        return dp[idx][amount]=min(a,b);
+        int inc=INT_MAX-1;
+        if(coins[idx]<=amount) inc=1+h(idx,amount-coins[idx],coins,dp);
+        int exc=h(idx-1,amount,coins,dp);
+        return dp[idx][amount]=min(inc,exc);
     }
     int coinChange(vector<int>& coins, int amount) {
         sort(coins.begin(),coins.end());
         int n=coins.size();
         vector<vector<int>> dp(n+1,vector<int> (amount+1,-1));
-        int ans=h(n-1,amount,coins,dp);
-       
-        return ans==INT_MAX-1?-1:ans;
+        int x= h(n-1,amount,coins,dp);
+        return x==INT_MAX-1? -1: x;
     }
 };
