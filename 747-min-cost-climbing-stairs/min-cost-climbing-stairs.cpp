fb6a5803528f14328bd0bb4cd78112164b2d4e70
notsahill <90394600+notsahill@users.noreply.github.com>
2023-10-13 09:43:33 +0530

    Time: 10 ms (13.04%) | Memory: 13.9 MB (37.30%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int helper(int idx,vector<int> &cost,vector<int> &dp)
+    int dp[1002];
+    int h(int idx,vector<int> &cost)
     {
         if(idx>=cost.size()) return 0;
         if(dp[idx]!=-1) return dp[idx];
-        int a=cost[idx]+helper(idx+1,cost,dp);
-        int b=cost[idx]+helper(idx+2,cost,dp);
+        int a=cost[idx]+h(idx+1,cost);
+        int b=cost[idx]+h(idx+2,cost);
         return dp[idx]=min(a,b);
     }
-    int minCostClimbingStairs(vector<int>& cost) 
-    {
-        vector<int> dp(cost.size(),-1);
-        return min(helper(0,cost,dp),helper(1,cost,dp));
+    int minCostClimbingStairs(vector<int>& cost) {
+        memset(dp,-1,sizeof(dp));
+        return min(h(0,cost),h(1,cost));
     }
 };
