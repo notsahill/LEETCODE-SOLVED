6be80e9a07d14ec6b084a8d96f26548935ec5e7e
notsahill <90394600+notsahill@users.noreply.github.com>
2023-10-26 02:54:59 +0530

    Time: 0 ms (100.00%) | Memory: 6.6 MB (12.59%) - LeetSync

---
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int kthGrammar(int n, int k) {
+        if(k==1 and n==1) return 0;
+        int mid=pow(2,n-1)/2;
+        if(k<=mid) return kthGrammar(n-1,k);
+        return !kthGrammar(n-1,k-mid);
+    }
+};
