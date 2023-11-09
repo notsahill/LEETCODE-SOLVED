e82fee4134005eb97b1f87af4fadc12696d6405c
notsahill <90394600+notsahill@users.noreply.github.com>
2023-11-09 11:14:11 +0530

    Time: 44 ms (55.10%) | Memory: 19.7 MB (27.09%) - LeetSync

--- 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.cpp
+++ 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
     int distinctPrimeFactors(vector<int>& nums) {
+        int primes[1001];
+        memset(primes,1,sizeof(primes));
+        for(int i=2;i<=1000;i++)
+        {
+            if(primes[i])
+            {
+                for(int j=2*i;j<=1000;j+=i)
+                {
+                    primes[j]=0;
+                }
+            }
+        }
+        vector<int> v;
+        for(int i=2;i<=1000;i++)
+        {
+            if(primes[i]) v.push_back(i);
+        }
         set<int> s;
         for(auto a:nums)
         {
-            int j=2;
+            int j=0;
             while(a!=1)
             {
-                if(a%j==0)
+                if(a%v[j]==0)
                 {
-                    a=a/j;
-                    s.insert(j);
+                    a=a/v[j];
+                    s.insert(v[j]);
                 }
                 else j++;
             }
