c3784da971a8af493f842a7a9d4c6601f7b5ce06
notsahill <90394600+notsahill@users.noreply.github.com>
2023-11-26 01:36:34 +0530

    Time: 350 ms (10.00%) | Memory: 37.4 MB (20.00%) - LeetSync

--- 3209-minimum-number-of-coins-for-fruits/minimum-number-of-coins-for-fruits.cpp
+++ 3209-minimum-number-of-coins-for-fruits/minimum-number-of-coins-for-fruits.cpp
@@ -6,7 +6,6 @@ public:
         int n=prices.size();
         upto=min(upto,n);
         if(idx==n) return 0;
-        if(idx>n) return 1e9;
         if(idx>upto) return prices[idx] +h(idx+1,idx+idx+1,prices);
         if(dp[idx][upto]!=-1) return dp[idx][upto];
         int buy=prices[idx]+ h(idx+1,idx+idx+1,prices);
