bd7e3a4a6b433cdf642ad97730747fa7b9bf86ae
notsahill <90394600+notsahill@users.noreply.github.com>
2023-12-18 16:35:17 +0530

    Time: 573 ms (6.28%) | Memory: 178.2 MB (17.87%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
@@ -4,22 +4,19 @@ class FoodRatings {
 
 public:
     FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
-        for (int i = 0; i < foods.size(); i++) {
-            foodToCuisineAndRating[foods[i]] = {cuisines[i], ratings[i]};
+        for(int i=0;i<foods.size();i++){
+            foodToCuisineAndRating[foods[i]]={cuisines[i],ratings[i]};
             cuisineToRatingAndFoods[cuisines[i]][ratings[i]].insert(foods[i]);
         }
     }
-
     void changeRating(string food, int newRating) {
-        auto foodInfo = foodToCuisineAndRating.find(food);
-        string cuisine = foodInfo->second.first;
-        int oldRating = foodInfo->second.second;
-        foodInfo->second.second = newRating;
-
-        cuisineToRatingAndFoods[cuisine][oldRating].erase(food);
-        if (cuisineToRatingAndFoods[cuisine][oldRating].empty())
-            cuisineToRatingAndFoods[cuisine].erase(oldRating);
-
+        string cuisine=foodToCuisineAndRating[food].first;
+        int rating=foodToCuisineAndRating[food].second;
+        foodToCuisineAndRating[food]={cuisine,newRating};
+        cuisineToRatingAndFoods[cuisine][rating].erase(food);
+        if(cuisineToRatingAndFoods[cuisine][rating].size()==0){
+            cuisineToRatingAndFoods[cuisine].erase(rating);
+        }
         cuisineToRatingAndFoods[cuisine][newRating].insert(food);
     }
 
