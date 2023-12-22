4e4765ab2e4f68f4c96234fbf74fe36e05bdf9d2
notsahill <90394600+notsahill@users.noreply.github.com>
2023-12-22 16:19:26 +0530

    Time: 20 ms (8.99%) | Memory: 8.6 MB (34.77%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -12,10 +12,7 @@ public:
         int ans=0;
         for(int i=0;i<s.size();i++)
         {
-            if(mp[s[i]]<mp[s[i+1]])
-            {
-                ans-=mp[s[i]];
-            }
+            if(mp[s[i]]<mp[s[i+1]]) ans-=mp[s[i]];
             else ans+=mp[s[i]];
         }
         return ans;
