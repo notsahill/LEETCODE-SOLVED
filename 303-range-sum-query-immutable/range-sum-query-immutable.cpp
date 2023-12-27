6619db695a8cfceb30eeebf3a2ff7d247d7698c6
notsahill <90394600+notsahill@users.noreply.github.com>
2023-12-27 13:53:53 +0530

    Time: 16 ms (86.84%) | Memory: 17.4 MB (43.22%) - LeetSync

---
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -0,0 +1,21 @@
+class NumArray {
+public:
+    vector<int> pref;
+    NumArray(vector<int>& nums) {
+        int n=nums.size();
+        pref.resize(n+1);
+        pref[0]=0;
+        for(int i=1;i<=n;i++){
+            pref[i]+=pref[i-1]+nums[i-1];
+        }
+    }
+    int sumRange(int left, int right) {
+        return pref[right+1]-pref[left];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
