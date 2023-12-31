00d9ce75dda786bd740f590c3d0b3eea779c70b1
notsahill <90394600+notsahill@users.noreply.github.com>
2023-12-31 13:20:20 +0530

    Time: 3 ms (47.38%) | Memory: 6.7 MB (41.15%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
         int mx=-1;
+        map<int,int> mp;
         for(int i=0;i<s.size();i++){
-            for(int j=i+1;j<s.size();j++){
-                if(s[j]==s[i]){
-                    mx=max(mx,j-i-1);
-                }
+            if(mp.find(s[i])!=mp.end()){
+                mx=max(mx,i-mp[s[i]]-1);
             }
+            else mp[s[i]]=i;
         }
         return mx;
     }
