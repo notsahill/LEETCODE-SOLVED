edbcc439941e30371cd69a80c8f43de911a0e112
notsahill <90394600+notsahill@users.noreply.github.com>
2024-01-04 00:48:16 +0530

    Time: 4 ms (38.78%) | Memory: 7.6 MB (52.30%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void h(int idx,vector<int>& nums,vector<int>& temp)
-    {
-        if(idx>=nums.size())
-        {
+    void h(int i,vector<int> &temp,vector<int> &nums){
+        if(i>=nums.size()){
             ans.push_back(temp);
             return;
         }
-        temp.push_back(nums[idx]);
-        h(idx+1,nums,temp);
+        temp.push_back(nums[i]);
+        h(i+1,temp,nums);
         temp.pop_back();
-        h(idx+1,nums,temp);
+        h(i+1,temp,nums);
     }
-    vector<vector<int>> subsets(vector<int>& nums) 
-    {
+    vector<vector<int>> subsets(vector<int>& nums) {
         vector<int> temp;
-        h(0,nums,temp);
+        h(0,temp,nums);
         return ans;
     }
 };
