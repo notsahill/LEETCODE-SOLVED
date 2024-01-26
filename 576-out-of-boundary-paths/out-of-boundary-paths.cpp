5a6fdbe8bf5be9ef612754b2a9f0cd785ca04ed7
notsahill <90394600+notsahill@users.noreply.github.com>
2024-01-26 14:13:35 +0530

    Time: 7 ms (59.64%) | Memory: 10.8 MB (22.69%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/out-of-boundary-paths.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int dp[52][52][52];
-    int h(int i,int j,int &m, int &n, int move){
+    int h(int i,int j,int &m, int &n, int move,vector<vector<vector<int>>> &dp){
         int MOD=1e9+7;
         if((i<0 or j<0 or i>=m or j>=n) and move>=0 ) return 1;
         if(move<0) return 0;
         if(dp[i][j][move]!=-1) return dp[i][j][move];
-        long long a=h(i-1,j,m,n,move-1)%MOD;
-        long long b=h(i+1,j,m,n,move-1)%MOD;
-        long long c=h(i,j-1,m,n,move-1)%MOD;
-        long long d=h(i,j+1,m,n,move-1)%MOD;
+        long long a=h(i-1,j,m,n,move-1,dp)%MOD;
+        long long b=h(i+1,j,m,n,move-1,dp)%MOD;
+        long long c=h(i,j-1,m,n,move-1,dp)%MOD;
+        long long d=h(i,j+1,m,n,move-1,dp)%MOD;
         long long ans=(a+b+c+d)%MOD;
         return dp[i][j][move]=ans;
     }
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        memset(dp,-1,sizeof(dp));
-        return h(startRow,startColumn,m,n,maxMove);
+        vector<vector<vector<int>>> dp(m+1,vector<vector<int>> (n+1,vector<int>(maxMove+1,-1)));
+        return h(startRow,startColumn,m,n,maxMove,dp);
     }
 };
