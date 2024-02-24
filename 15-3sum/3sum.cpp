cc95e7b08d545171cbf04c7fc8b5d8ea970c1f42
notsahill <90394600+notsahill@users.noreply.github.com>
2024-02-24 20:40:46 +0530

    Time: 61 ms (84.72%) | Memory: 27 MB (58.87%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -4,18 +4,19 @@ public:
         vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        for(int i=0;i<nums.size()-2;i++){
-            if(i>0 and nums[i]==nums[i-1]) continue;
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
             int j=i+1,k=n-1;
-            while(j<k){
-                if(nums[i]+nums[j]+nums[k]>0) k--;
-                else if(nums[i]+nums[j]+nums[k]<0) j++;
-                else{
+            while(k>j){
+                long long sum=nums[i]+nums[j]+nums[k];
+                if(sum==0){
                     ans.push_back({nums[i],nums[j],nums[k]});
                     j++,k--;
-                    while(j<n && nums[j]==nums[j-1]) j++;
-                    while(k>j && nums[k]==nums[k+1]) k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(nums[k]==nums[k+1] && k>j) k--;
                 }
+                else if(sum>0) k--;
+                else j++;
             }
         }
         return ans;
