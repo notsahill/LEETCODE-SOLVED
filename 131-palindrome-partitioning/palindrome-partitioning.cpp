9d6a30fbe51ea468ec94ad5c145172f4b7b01cf8
notsahill <90394600+notsahill@users.noreply.github.com>
2024-02-25 16:39:32 +0530

    Time: 78 ms (83.73%) | Memory: 79.2 MB (31.74%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
     vector<vector<string>> ans;
-    bool isPalindrome(string s)
-    {
+    bool isPalindrome(string &s){
         int i=0,j=s.size()-1;
-        while(i<=j)
-        {
+        while(i<j){
             if(s[i]!=s[j]) return false;
             i++,j--;
         }
         return true;
     }
-    void h(int idx,vector<string> &temp,string &s)
-    {
-        if(idx>=s.size())
-        {
+    void h(int idx,vector<string> &temp,string &s){
+        if(idx>=s.size()){
             ans.push_back(temp);
             return;
         }
         string str="";
-        for(int i=idx;i<s.size();i++)
-        {
+        for(int i=idx;i<s.size();i++){
             str+=s[i];
-            if(isPalindrome(str))
-            {
+            if(isPalindrome(str)){
                 temp.push_back(str);
                 h(i+1,temp,s);
                 temp.pop_back();
             }
         }
     }
-    vector<vector<string>> partition(string s) 
-    {
+    vector<vector<string>> partition(string s) {
         vector<string> temp;
         h(0,temp,s);
         return ans;
