6fc4921d723e53102c2a370a45748c8bfbafca9e
notsahill <90394600+notsahill@users.noreply.github.com>
2024-03-11 13:54:27 +0530

    Time: 5 ms (8.56%) | Memory: 7.3 MB (83.77%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
+        //giving maximum preference to charcters that appear first in order
         vector<int> preference(26,0);
         int x=26;
         for(char ch:order){
             preference[ch-'a']=x;
             x--;
         }
+
+        //apply bubble sort
         for(int i=0;i<s.size();i++){
             for(int j=i+1;j<s.size();j++){
                 if(preference[s[j]-'a']>preference[s[i]-'a']){
