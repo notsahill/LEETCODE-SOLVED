f5b3f54cdf962a6e039efa54e195be2b672ef6a9
notsahill <90394600+notsahill@users.noreply.github.com>
2024-03-11 13:41:08 +0530

    Time: 5 ms (8.56%) | Memory: 7.3 MB (83.77%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        vector<int> preference(26,0);
+        int x=26;
+        for(char ch:order){
+            preference[ch-'a']=x;
+            x--;
+        }
+        for(int i=0;i<s.size();i++){
+            for(int j=i;j<s.size();j++){
+                if(preference[s[j]-'a']>preference[s[i]-'a']){
+                    swap(s[i],s[j]);
+                }
+            }
+        }
+        return s;
+    }
+};
