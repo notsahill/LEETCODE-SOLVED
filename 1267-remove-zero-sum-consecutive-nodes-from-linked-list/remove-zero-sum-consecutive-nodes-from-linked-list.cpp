d56595d94aec859e0fc05bc101831a8877d3c0c3
notsahill <90394600+notsahill@users.noreply.github.com>
2024-03-13 00:05:36 +0530

    Time: 9 ms (42.96%) | Memory: 15 MB (36.48%) - LeetSync

--- 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.cpp
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -11,26 +11,28 @@
 class Solution {
 public:
     ListNode* removeZeroSumSublists(ListNode* head) {
-        ListNode* dummy= new ListNode(0);
-        dummy->next=head;
         int pref_sum=0;
-        map<int,ListNode*> mp;
-        mp[pref_sum]=dummy;
+        unordered_map<int,ListNode*> mp;
+        ListNode* dummy=new ListNode(-1);
+        dummy->next=head;
+        mp[0]=dummy;
+        
         while(head!=NULL){
             pref_sum+=head->val;
             if(mp.find(pref_sum)==mp.end()){
                 mp[pref_sum]=head;
+                head=head->next;
             }
             else{
-                ListNode* start=mp[pref_sum],*temp=start;
+                ListNode* curr=mp[pref_sum],*temp=curr;
                 while(temp!=head){
                     temp=temp->next;
                     pref_sum+=temp->val;
                     if(temp!=head) mp.erase(pref_sum);
                 }
-                start->next=head->next;
+                curr->next=head->next;
+                head=head->next;
             }
-            head=head->next;
         }
         return dummy->next;
     }
