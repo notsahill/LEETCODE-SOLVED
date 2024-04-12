76b0f85fed517260cfc7774cc4f831a08f4a81f1
notsahill <90394600+notsahill@users.noreply.github.com>
2024-04-12 18:53:07 +0530

    Time: 8 ms (73.68%) | Memory: 23.3 MB (13.23%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -5,10 +5,10 @@ public:
         vector<int> left(n),right(n);
         left[0]=height[0],right[n-1]=height[n-1];
         for(int i=1;i<n;i++){
-            left[i]=max(height[i],left[i-1]);
+            left[i]=max(left[i-1],height[i]);
         }
         for(int i=n-2;i>=0;i--){
-            right[i]=max(height[i],right[i+1]);
+            right[i]=max(right[i+1],height[i]);
         }
         int ans=0;
         for(int i=0;i<n;i++){
